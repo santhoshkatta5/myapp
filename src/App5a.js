53b718b299e3f89394f0b0a23b6a6df6df808d78
santhosh katta <santhoshkatta@lyrostech.com>
2024-03-09 14:02:07 +0530

    commit

--- src/App5a.js
+++ src/App5a.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function App5() {
+export default function App5a() {
     let names = ["john", "cathy", "trevor", "aaron"]
     return (<ul>
         {names.map((e, i) => (
