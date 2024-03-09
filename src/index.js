9c5fd8a4cf38bc178e396f2f662af52989aa3917
santhosh katta <santhoshkatta@lyrostech.com>
2024-03-09 16:26:33 +0530

    commmit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App15 from "./App15";
+import App16 from "./App16";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App3 names={["santhu","akhi","sankshya"]} marks={[80,70,100]} /> */}
-    <App15/>
+    <App16/>
   </React.StrictMode>
 );
